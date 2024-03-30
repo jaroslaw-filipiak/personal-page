43f6cd51b9d421995e295ed77f48390b470d87d1
jaroslaw-filipiak <info@j-filipiak.pl>
2024-03-30 21:18:11 +0100

    init

---
+++ src/app/o-mnie/page.tsx
@@ -0,0 +1,12 @@
+export default function AboutMe() {
+  return (
+    <>
+      <section id='dlaczego-ja' className='h-screen border border-white'>
+        11
+      </section>
+      <section id='co-robie' className='h-screen border border-white'>
+        22
+      </section>
+    </>
+  );
+}
