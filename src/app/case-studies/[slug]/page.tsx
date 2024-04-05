9cd924830b96be761968db234cd6f9d805d06384
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-05 06:26:32 +0200

    add img

---
+++ src/app/case-studies/[slug]/page.tsx
@@ -0,0 +1,104 @@
+import Nav from '@/components/Nav';
+import Link from 'next/link';
+import Cta from '@/components/Cta';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <section id='start' className='pt-72 pb-72 bg-white h-screen'>
+        <div className='flex items-center justify-center'>
+          <div className='container flex flex-col items-start'>
+            <div>
+              <h1 className='text-5xl text-balance pb-24 '>
+                Single case study
+              </h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='bg-white pt-20 pb-20'>
+        <div className='container '>
+          <h2 className='text-3xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            inventore dolor assumenda, ipsa, deserunt voluptatum quidem
+            laudantium ducimus omnis optio unde veritatis?
+          </h2>
+          <p className='text-lg mt-20 mb-20'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nisi
+            dolorem ab totam quod nihil, tempore repellendus, atque fugiat sed
+            laboriosam impedit voluptate maiores voluptatum eum ut dolores rem
+            quaerat facilis quos laborum sunt dolore? Quasi, sunt esse,
+            accusamus ducimus nihil fugiat delectus, asperiores distinctio magni
+            obcaecati inventore illum minus magnam iusto doloremque libero nemo
+            impedit non. Veniam, hic tempore. Eum commodi perferendis laborum
+            illo sed, itaque vitae cum impedit culpa sapiente officiis ab
+            officia voluptas vel maiores! Temporibus adipisci voluptatem vitae?
+            Harum sed vitae inventore, accusamus modi perspiciatis ab
+            praesentium eos libero ipsam consequuntur molestias tenetur eum
+            veniam odio.
+          </p>
+          <p className='text-lg mb-10'>Zakres pracy:</p>
+          <div className='flex w-full lg:w-10/12 gap-10 text-md mb-20'>
+            <div>
+              <h2>
+                Znamy narzędzia branżowe, jeśli chodzi o tworzenie dedykowanych
+                aplikacji (mobilnych i webowych). Oferujemy znacznie więcej niż
+                tylko umiejętności.
+              </h2>
+            </div>
+            <div>
+              Znamy narzędzia branżowe, jeśli chodzi o tworzenie dedykowanych
+              aplikacji (mobilnych i webowych). Oferujemy znacznie więcej niż
+              tylko umiejętności.
+            </div>
+          </div>
+          <Image
+            src='/img/img.jpg'
+            width={1422}
+            height={822}
+            alt='image'
+            className='mb-20'
+          ></Image>
+          <Image
+            src='/img/img.jpg'
+            width={1422}
+            height={822}
+            alt='image'
+          ></Image>
+        </div>
+      </section>
+
+      <section className='bg-white pt-20 pb-20'>
+        <div className='container'>
+          <h2 className='text-3xl'>Podsumowanie</h2>
+          <p className='text-lg mt-10 mb-32'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nisi
+            dolorem ab totam quod nihil, tempore repellendus, atque fugiat sed
+            laboriosam impedit voluptate maiores voluptatum eum ut dolores rem
+            quaerat facilis quos laborum sunt dolore? Quasi, sunt esse,
+            accusamus ducimus nihil fugiat delectus, asperiores distinctio magni
+            obcaecati inventore illum minus magnam iusto doloremque libero nemo
+            impedit non. Veniam, hic tempore. Eum commodi perferendis laborum
+            illo sed, itaque vitae cum impedit culpa sapiente officiis ab
+            officia voluptas vel maiores! Temporibus adipisci voluptatem vitae?
+            Harum sed vitae inventore, accusamus modi perspiciatis ab
+            praesentium eos libero ipsam consequuntur molestias tenetur eum
+            veniam odio.
+          </p>
+        </div>
+      </section>
+
+      <section className='bg-lightGray pt-20 pb-20'>
+        <div className='container flex items-center justify-center'>
+          <div className='prev'>prev</div>
+          <div className='title flex-1 text-center text-lg'>
+            Następny projekt: <strong>Lorem, ipsum dolor.</strong>
+          </div>
+          <div className='next'>next</div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/o-mnie/page.tsx
+++ src/app/o-mnie/page.tsx
@@ -1,12 +1,98 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Cta from '@/components/Cta';
+
 export default function AboutMe() {
   return (
     <>
-      <section id='dlaczego-ja' className='h-screen border bg-white'>
-        11
+      <section
+        id='about'
+        className='h-screen flex items-center justify-center bg-dark text-white'
+      >
+        <div className='container'>
+          <h1 className='text-5xl text-balance'>
+            Nazywam się Jarosław Filipiak, jestem doświadczonym grafikiem oraz
+            front-end developerem. Specjalizuje się w projektowaniu oraz
+            kodowaniu stron internetowych a także kompleksowych aplikacji
+            webowych.
+          </h1>
+        </div>
       </section>
-      <section id='co-robie' className='h-screen border bg-white'>
-        22
+      <section id='skills' className='pt-72 pb-72 bg-white'>
+        <div className='flex items-center justify-center'>
+          <div className='container flex flex-col items-end'>
+            <div>
+              <h2 className='text-5xl text-balance pb-24'>
+                Posiadam solidny zestaw umiejętności i technologii związanych z
+                całą otoczką procesu projektowania.
+              </h2>
+            </div>
+
+            <div className='flex items-start gap-10 '>
+              <div>
+                <h3 className='text-lg'>
+                  Rozpoczynałem swoją przygodę jako grafik komputerowy, w
+                  trakcie ponad 10 letniej przygody zdobyłem kompetencje, które
+                  doprowadziły mnie do pracy jako front-end developer. A to
+                  jeszcze nie koniec mojej przygody. Uważam, że człowiek musi
+                  się rozwijać, bo gdy się nie rozwija to tak naprawdę się
+                  cofa... ponieważ świat idzie do przodu.
+                </h3>
+              </div>
+              <div className='text-sm'>
+                <p className='mb-10'>
+                  Doświadczenie zdobywałem zarówno w większych agencjach jak i
+                  obsługując klientów “mniej technicznych”Uważnie wsłuchuję się
+                  w potrzeby każdego klienta. Pracując jako freelancer mam tę
+                  swobodę że mogę zasugerować więcej różnych rozwiązań/wariantów
+                  wdrożenia projektu informatycznego aniżeli agencja, która
+                  jednak jest mocno zunifikowana na jedym stacku
+                  technologicznym.
+                </p>
+                <p>
+                  Oferuje indywidualne , partnerskie podejście , ogromne
+                  doświadczenie oraz jasne warunki współpracy
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
+
+      <section id='skills' className='pb-72 bg-white'>
+        <div className='flex items-center justify-center'>
+          <div className='container flex flex-col items-start'>
+            <div>
+              <h3 className='text-5xl text-balance pb-24 hidden'>
+                Mój największy kapitał to ludzie z którymi pracuje
+              </h3>
+            </div>
+
+            <div className='flex items-end gap-10'>
+              <div>
+                <h4 className='text-lg'>
+                  Jestem freelancerem i współpracuje blisko z klientem. Nie
+                  mniej posiadam kameralne grono osób z którymi współpracuje.
+                  Mam “pod ręką” kilka świetnych osób, które pomagają mi w
+                  codziennej pracy. Potrzebujesz fotografa ? montazysty ? osoby
+                  do konsultacji od AI ? Do wiekszych lub bardziej wymagających
+                  projektów jestem w stanie utowrzyć wiekszy zespół.
+                </h4>
+              </div>
+            </div>
+
+            <div className='mt-24'>
+              <Image
+                src='/img/jaroslaw-filipiak.webp'
+                alt='Jarosław Filipiak'
+                width={434}
+                height={688}
+              ></Image>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Cta />
     </>
   );
 }
