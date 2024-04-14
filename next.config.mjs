139ea5849476abc50c564be33c60e29b5b736945
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-14 13:36:16 +0200

    remove basepath in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/j-filipiak.pl',
-  assetPrefix: '/j-filipiak.pl',
 };
 
 export default nextConfig;
