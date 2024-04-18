a1a956c6c91a9c2c2fd0c9ddce30fca2f20bc0ff
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-18 17:30:10 +0200

    blog posts markup more semantic

--- src/components/blog/BlogHero.tsx
+++ src/components/blog/BlogHero.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { motion } from 'framer-motion';
 export default function BlogHero() {
   return (
-    <div className='pt-32 lg:pt-40 2xl:pt-72 lg:pb-40 2xl:pb-72  min-h-screen flex items-center justify-center'>
-      <h1 className='text-4xl font-bold text-center text-dark'>Blog</h1>
-    </div>
+    <section
+      id='start'
+      className='pt-32 lg:pt-40 2xl:pt-72 pb-40 lg:pb-40 2xl:pb-72  '
+    >
+      <div className='flex items-center justify-center'>
+        <motion.div
+          initial={{ opacity: 0, top: 30 }}
+          whileInView={{ opacity: 1, top: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ type: 'spring', duration: 1 }}
+          className='container flex flex-col items-start relative  '
+        >
+          <div>
+            <h1 className='text-5xl text-balance pb-10'>Blog</h1>
+          </div>
+        </motion.div>
+      </div>
+    </section>
   );
 }
--- src/components/blog/BlogPosts.tsx
+++ src/components/blog/BlogPosts.tsx
@@ -14,25 +14,32 @@ export default async function BlogPosts() {
   const data = await fetchPosts();
 
   return (
-    <div>
-      <div className='flex gap-10'>
-        {data.map((post: any) => (
-          <div className='border p-5' key={post.id}>
-            <h2>{post.title.rendered}</h2>
+    <section className='container grid gap-4 grid-cols-1 lg:grid-cols-2 pb-10 lg:pb-24'>
+      {data.map((post: any) => (
+        <article className='p-5 group' key={post.id}>
+          <Link href={`/blog/${post.slug}`}>
             <img
+              loading='lazy'
+              className='mb-3 group-hover:opacity-65 transition-all opacity-100'
               src={
                 post.featured_media_src_url ? post.featured_media_src_url : ''
               }
-              alt=''
+              alt={post.title.rendered}
             />
-            <p>
-              {post.featured_media_src_url ? post.featured_media_src_url : ''}
-            </p>
-            <p className='hidden'>{post.content.rendered}</p>
-            <Link href={`/blog/${post.slug}`}>Read more</Link>
-          </div>
-        ))}
-      </div>
-    </div>
+
+            <div className='max-w-[610px] flex flex-col gap-3'>
+              <div className='flex flex-col'>
+                <h3 className='text-2xl'>{post.title.rendered}</h3>
+                <h4 className='text-lg'>{post?.acf?.subtitle}</h4>
+              </div>
+              <h5
+                dangerouslySetInnerHTML={{ __html: post?.excerpt?.rendered }}
+                className='text-md'
+              ></h5>
+            </div>
+          </Link>
+        </article>
+      ))}
+    </section>
   );
 }
