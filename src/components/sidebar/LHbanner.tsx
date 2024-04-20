4c1ce98c2b164d9a88d59c4c6b74d6ffcd97992f
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-20 18:10:25 +0200

    dot jpg

--- src/components/sidebar/LHbanner.tsx
+++ src/components/sidebar/LHbanner.tsx
@@ -4,7 +4,7 @@ export default function LHbanner() {
   return (
     <div>
       <Image
-        src='img/sidebar/lh-promo/jpg'
+        src='img/sidebar/lh-promo.jpg'
         width={410}
         height={190}
         alt='Hosting Mango od lh.pl - Najlepszy start dla Ciebie i Twojej strony!'
