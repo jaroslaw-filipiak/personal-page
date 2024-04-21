a392c339102459fa5f8fa9ff7f6868c21dea0f40
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-21 10:07:44 +0200

    upate sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,17 +7,30 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   );
   const posts = await res.json();
 
-  const postsEntries: MetadataRoute.Sitemap = posts.map(({ slug }) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${slug}`,
-    // lastModified: new Date(),
-    // changeFrequency: 'yearly',
-    //=== priority: 1,
-  }));
+  const postsEntries: MetadataRoute.Sitemap = posts.map(
+    ({ slug, modified }) => ({
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${slug}`,
+      lastModified: modified,
+      priority: 1,
+    })
+  );
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/kontakt`,
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/o-mnie`,
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/polityka-prywatnosci`,
       priority: 1,
     },
     ...postsEntries,
