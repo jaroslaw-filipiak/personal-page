de94cf7bf954ea0daa0e9608c2dee75da09f044b
jaroslaw-filipiak <info@j-filipiak.pl>
2024-04-21 08:38:50 +0200

    dynamic sitemap with postEntries

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,17 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/wp-json/wp/v2/posts`
+  );
+  const posts = await res;
+
+  const postsEntries: MetadataRoute.Sitemap = posts.map(({ slug }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${slug}`,
+    // lastModified: new Date(),
+    // changeFrequency: 'yearly',
+    //=== priority: 1,
+  }));
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
@@ -8,5 +19,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'yearly',
       priority: 1,
     },
+    ...postsEntries,
   ];
 }
