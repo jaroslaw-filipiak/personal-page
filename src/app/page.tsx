a2b990899e785168e4a51b4622cfeaa7c10ceb88
jaroslaw-filipiak <info@j-filipiak.pl>
2024-06-09 14:54:09 +0200

    fix    "websiteCreationOfferSchema" is not a valid Page export field.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
         <script
           type='application/ld+json'
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify(websiteCreationOfferSchema),
+            __html: JSON.stringify(jsonLd),
           }}
         />
       </Head>
