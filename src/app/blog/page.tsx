5fcc5c940e8861df683e1968d150d2e8d8377b66
jaroslaw-filipiak <info@j-filipiak.pl>
2024-08-09 19:43:37 +0200

    add blog page schema + metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,40 @@ import Nav from '@/components/Nav';
 import BlogHero from '@/components/blog/BlogHero';
 import BlogPosts from '@/components/blog/BlogPosts';
 import { Blog, WithContext } from 'schema-dts';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title:
+    'Blog o tworzeniu nowoczesnych stron internetowych WordPress, Node.js, Vue.js, Next.js - j-filipiak.pl',
+  description:
+    'Tworzenie stron internetowych WordPress, Vue.js, Automatyzacja procesw firmowych, praktyki SEO oraz o wiele więcej',
+  keywords: [
+    'projektowanie stron www w Figmie',
+    'projektowanie stron internetowych',
+    'auktorskie kodowanie stron www , html, js, css, sass, webpack, vite.js, next.js, vue.js, wordpress, node.js, dedykowane oprogramowanie dla firm, aplikacje webowe spa, aplikacje webowe pwa, aplikacje webowe ssr, szybkie strony internetowe',
+    'Webpack',
+    'Next.js',
+    'Node.js',
+    'WordPress',
+    'Google Lighthouse',
+    'Dedykowane oprogramowanie dla firm',
+    'Aplikacje webowe SPA',
+    'Aplikacje webowe PWA',
+    'Aplikacje webowe SSR',
+    'Szybkie strony internetowe',
+  ],
+  generator: 'Next.js',
+  openGraph: {
+    type: 'website',
+    url: 'https://j-filipiak.pl/blog',
+    title:
+      'Blog o tworzeniu nowoczesnych stron internetowych WordPress, Node.js, Vue.js, Next.js - j-filipiak.pl',
+    description:
+      'Tworzenie stron internetowych WordPress, Vue.js, Automatyzacja procesw firmowych, praktyki SEO oraz o wiele więcej',
+    images: ['ogimage-contact.jpg'],
+    // TODO: og images per page
+  },
+};
 
 export default function blogPage() {
   const blogSchema: WithContext<Blog> = {
