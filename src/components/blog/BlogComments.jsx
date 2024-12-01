b8729e43731023397cbf1224b8ff9d0ba912dfa9
jaroslaw-filipiak <info@j-filipiak.pl>
2024-12-01 19:11:46 +0100

    add comments form

--- src/components/blog/BlogComments.jsx
+++ src/components/blog/BlogComments.jsx
@@ -6,6 +6,25 @@ import { motion } from 'framer-motion';
 export default function BlogComments(props) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+
+  const clearForm = () => {
+    document.getElementById('comment').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('www').value = '';
+  };
+
+  const thanksMessages = [
+    'Dziękuję za Twój komentarz! To właśnie dzięki takim interakcjom wiem, że warto poświęcać czas na pisanie bloga.',
+    'Cieszę się, że podzieliłeś/aś się swoją opinią. Każdy komentarz jest dla mnie ważny i staram się odpowiadać na każdego.',
+    'Wspaniale, że dołączasz do dyskusji! Twój głos jest ważną częścią tego bloga. Dziękuję!',
+    'Dzięki za komentarz! To dzięki takim czytelnikom jak Ty blog się rozwija i daje mi kopniaka do działania.',
+    'Bardzo doceniam Twój wkład w dyskusję. Dziękuję za poświęcony czas!',
+  ];
+
+  const randomMessage =
+    thanksMessages[Math.floor(Math.random() * thanksMessages.length)];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,7 +44,7 @@ export default function BlogComments(props) {
 
     try {
       const response = await fetch(
-        `https://j-filipiak.pl/api/wp-json/wp/v2/comments?post={props.postId}&content=${data.content}&author_name=${data.author_name}&author_email=${data.author_email}&author_url=${data.author_url}`,
+        `https://j-filipiak.pl/api/wp-json/wp/v2/comments?post=${props.postId}&content=${data.content}&author_name=${data.author_name}&author_email=${data.author_email}&author_url=${data.author_url}`,
         {
           method: 'POST',
           headers: {
@@ -39,18 +58,23 @@ export default function BlogComments(props) {
         }
       );
 
-      console.log(response);
-
-      if (!response.ok) {
-        throw new Error('Failed to submit form');
+      if (response.status === 201) {
+        console.log('Comment added');
+        setSuccess(true);
       }
 
-      // Reset form
-      e.currentTarget.reset();
+      setError(null);
+      clearForm();
     } catch (err) {
+      console.log(err);
       setError(err instanceof Error ? err.message : 'Something went wrong');
     } finally {
       setLoading(false);
+      setError(null);
+
+      setTimeout(() => {
+        setSuccess(false);
+      }, 9000);
     }
   };
 
@@ -133,13 +157,27 @@ export default function BlogComments(props) {
                 />
               </div>
 
-              <button
-                type='submit'
-                disabled={loading}
-                className='bg-dark hover:opacity-80 transition-all text-white block pt-5 pb-5 pr-10 pl-10  items-center justify-center text-center'
-              >
-                {loading ? 'Wysyłanie...' : 'Dodaj komentarz'}
-              </button>
+              <div className='flex items-start justify-around gap-10 mt-4'>
+                <button
+                  type='submit'
+                  disabled={loading}
+                  className='bg-dark hover:opacity-80 transition-all text-white block pt-5 pb-5 pr-10 pl-10  items-center justify-center text-center w-[220px] min-w-[220px] max-w-[220px]'
+                >
+                  {loading ? 'Wysyłanie...' : 'Dodaj komentarz'}
+                </button>
+
+                <div>
+                  {success && (
+                    <div>
+                      <div className='text-base'>{randomMessage}</div>
+                      <div className='text-md'>
+                        (Twój komentarz aktualnie czeka na moderację także
+                        pojawi się po zatwierdzeniu)
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
             </form>
           </div>
         </motion.div>
