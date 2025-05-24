216426d9df5870a1414ced25c3f164886a873cc4
jaroslaw-filipiak <info@j-filipiak.pl>
2025-05-24 15:50:47 +0200

    framer motion test

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { motion } from 'motion/react';
+
 export default function HomeHero() {
   return (
     <section
@@ -15,16 +17,35 @@ export default function HomeHero() {
       <div className='flex items-center justify-center'>
         <div className='container flex flex-col items-start relative'>
           <div className='sm:w-9/12'>
-            <h1 className='text-5xl text-balance pb-10 '>
+            <motion.h1
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              className='text-5xl text-balance pb-10 '
+            >
               Profesjonalne strony internetowe, które przyciągają klientów
-            </h1>
+            </motion.h1>
           </div>
 
           <div className='flex flex-col lg:flex-row lg:w-9/12 gap-10 text-lg'>
             <div>
-              <h2>Strony internetowe oparte min. o system WordPress.</h2>
-              <h3>Optymalizacja SEO</h3>
-              <h3>Piekielnie szybkie i stabilne strony</h3>
+              <motion.h2
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+              >
+                Strony internetowe oparte min. o system WordPress.
+              </motion.h2>
+              <motion.h3
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+              >
+                Optymalizacja SEO
+              </motion.h3>
+              <motion.h3
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+              >
+                Piekielnie szybkie i stabilne strony
+              </motion.h3>
             </div>
           </div>
         </div>
